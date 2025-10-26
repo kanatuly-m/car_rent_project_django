6dd748eb135bab569e5e2d1bee4492ee35bdc3b4
kanatuly-m <240941@astanait.edu.kz>
2025-10-26 15:01:25 +0500

    make another changes

--- rental/users/urls.py
+++ rental/users/urls.py
@@ -2,9 +2,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from users import views as user_views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='user-profile'),
+    path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('', user_views.home, name='home'),  
 ]
--- rental/users/views.py
+++ rental/users/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm
 from django.contrib.auth.forms import UserCreationForm
 from cars.models import Rental
+from django.contrib.auth import login
+from cars.models import Car
 
 
 
@@ -10,12 +12,18 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('login')  # Перенаправляем на страницу логина
+            user = form.save()
+            login(request, user)  # Авторизуем пользователя
+            return redirect('home')  # Переход на главную страницу после регистрации
     else:
         form = UserCreationForm()
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, 'registration/register.html', {'form': form})
 
 def profile(request):
     rentals = Rental.objects.filter(user=request.user)  # Предполагается, что у Rental есть поле "user"
     return render(request, 'users/profile.html', {'rentals': rentals})
+
+
+def home(request):
+    cars = Car.objects.all()  # Получаем список автомобилей
+    return render(request, 'home.html', {'cars': cars})
